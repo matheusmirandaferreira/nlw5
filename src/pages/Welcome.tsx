3ffcb4efd4b39bd1cd282773124c8436f0a87e69
Matheus <matheus@esgn.com.br>
2021-04-20 08:00:24 -0300

    NLW 5 - Trilha React Native - Aula 1

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  Text,
-  Image,
-  TouchableOpacity,
-  StyleSheet,
-} from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, Text, Image, StyleSheet } from 'react-native';
 
 import wateringImg from '../assets/watering.png';
+import { Button } from '../components/Button';
 import colors from '../styles/colors';
 
 export function Welcome() {
+  const [visible, setVisible] = useState(false);
+
+  function hadleVisible() {
+    visible ? setVisible(false) : setVisible(true);
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>
         Gerencie {'\n'} suas plantas {'\n'}de forma fácil
       </Text>
-      <Image style={styles.image} source={wateringImg} />
+      {visible && <Image style={styles.image} source={wateringImg} />}
       <Text style={styles.subtitle}>
         Não esqueca mais de regar suas plantas. Nós cuidamos de lembrar você
         sempre que precisar
       </Text>
-      <TouchableOpacity activeOpacity={0.8} style={styles.button}>
-        <Text style={styles.buttontext}>&gt;</Text>
-      </TouchableOpacity>
+
+      <Button title=">" onPress={hadleVisible} />
     </SafeAreaView>
   );
 }
@@ -45,7 +45,7 @@ const styles = StyleSheet.create({
 
   image: {
     width: 292,
-    height: 284
+    height: 284,
   },
 
   subtitle: {
@@ -54,19 +54,4 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     color: colors.heading,
   },
-
-  button: {
-    backgroundColor: colors.green,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 16,
-    marginBottom: 10,
-    height: 56,
-    width: 56,
-  },
-
-  buttontext: {
-    color: colors.white,
-    fontSize: 24
-  }
 });
